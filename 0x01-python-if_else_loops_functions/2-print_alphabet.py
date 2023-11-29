8f47d8af0d8641535b15d8268c36fa1670d8d197
ricardovolvox <ricardo.volvox@gmail.com>
2023-11-29 14:37:10 -0800

    ush

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 for character in range(97, 122 + 1):
-    print('{}'.format(chr(character)), end='')
+    if chr(character) !='q' and chr(character) !='e':
+        print('{}'.format(chr(character)), end='')
