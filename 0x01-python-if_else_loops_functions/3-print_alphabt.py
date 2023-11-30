ec10ccac42b8c7edf1122cf57c09e3e745deb233
ricardovolvox <ricardo.volvox@gmail.com>
2023-11-29 16:21:39 -0800

    files

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for character in range(97, 122 + 1):
-    if chr(character) !='q' and chr(character) !='e':
-        print('{}'.format(chr(character)), end='')
+for i in range(97, 122 + 1):
+    if chr(i) not in ['q', 'e']:
+        print("{0}" .format(chr(i)), end='')
