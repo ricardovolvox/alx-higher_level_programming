2cb0c4fd6f7f62aec1089127dc1a5876f297f7dc
ricardovolvox <ricardo.volvox@gmail.com>
2023-12-01 15:23:53 -0800

    push file 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 argument:")
     else:
-        print(f"arguments: {count}")
+        print(f"{count} arguments:")
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
